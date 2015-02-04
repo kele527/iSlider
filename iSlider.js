a6f0a059f5d2b12781a24ce38790ddc6d87f9a3d
kele527 <ycommon@qq.com>
2015-02-04 13:53:13 +0800

    增加一些功能

--- iSlider.js
+++ iSlider.js
@@ -5,6 +5,8 @@
  * @param {string} opts.wrap='.wrap' 容器 
  * @param {string} opts.item='.item'  滚动单元的元素
  * @param {string} opts.playClass='play'  触发播放动画的class
+ * @param {number} [opts.index=0]  设置初始显示的页码
+ * @param {number} [opts.noslide=[0,3,4]]  设置禁止滑动的页码, 禁止后 需要开发者手动绑定页面中的某个按钮事件进行滑动 
  * @param {number} [opts.speed=400] 动画速度 单位:ms
  * @param {number} [opts.triggerDist=30] 触发滑动的手指移动最小位移 单位:像素
  * @param {boolean} [opts.isVertical=true] 滑动方向 是否是垂直方向 默认是.
@@ -57,7 +59,7 @@
             }
         }
     });
-
+    demo http://kele527.github.io/iSlider/demo1.html
 
  * @date 2014/11/3 星期一
  * @author rowanyang
@@ -66,11 +68,14 @@
 
 ;(function (win) {
 
-var raf = function (cb) {setTimeout(function (){cb()},100)};
+var raf = function (cb) {setTimeout(function (){cb()},150)};
 //android上用了raf也没啥效果  所以只对高富帅用
 if (/iPhone|iPod|iPad/.test(navigator.userAgent)) {
     raf = window.requestAnimationFrame || window.webkitRequestAnimationFrame || raf;
 }
+
+//记录当前浏览的页码,  后退的时候用
+var sessionKey = location.host+location.pathname;
 
 var iSlider = function (opts) {
     this.opts={
@@ -80,6 +85,7 @@ var iSlider = function (opts) {
         speed:400, //滑屏速度 单位:ms
         triggerDist:30,//触发滑动的手指移动最小位移 单位:像素
         isVertical:true,//垂直滑还是水平滑动
+        useACC:true, //是否启用硬件加速 默认启用
         loadingImgs:[], //loading 预加载图片地址列表
         preLoadingImgs:[],
         onslide:function (index) {},//滑动回调 参数是本对象
@@ -118,6 +124,9 @@ iSlider.prototype={
     },
 	init:function () {
         var self = this;
+        //使用sessionStorage来保存当前浏览到第几页了   后退回来的时候 定位到这一页
+        this.index=parseInt(sessionStorage[sessionKey]) || this.opts.index || 0;
+
         this.wrap=this.$(this.opts.wrap);
 
         this.tpl= this.wrap.cloneNode(true);
@@ -129,15 +138,16 @@ iSlider.prototype={
         this.totalDist = 0,//移动的总距离
         this.deltaX1 = 0;//每次移动的正负
         this.deltaX2 = 0;//每次移动的正负
-        
-        this.displayWidth = document.documentElement.clientWidth; //图片区域最大宽度
-        this.displayHeight = document.documentElement.clientHeight; //图片区域最大高度
+
+        this.displayWidth = this.wrap.clientWidth; //滑动区域最大宽度
+        this.displayHeight = this.wrap.clientHeight; //滑动区域最大高度
 
         this.scrollDist=this.opts.isVertical ? this.displayHeight : this.displayWidth;//滚动的区域尺寸 
         
         this.wrap.innerHTML=
-            '<div id="current" class="js-iSlider-item '+this.tpl[0].className+'" style="'+this.getTransform(0)+'">'+this.tpl[0].innerHTML+'</div>'+
-            '<div id="next" class="js-iSlider-item '+this.tpl[1].className+'" style="'+this.getTransform('100%')+'">'+this.tpl[1].innerHTML+'</div>';
+            (this.index>0?'<div id="i-prev" class="js-iSlider-item '+this.tpl[this.index-1].className+'" style="'+this.getTransform('-100%')+'">'+this.tpl[this.index-1].innerHTML+'</div>':'')+
+            '<div id="i-current" class="js-iSlider-item '+this.tpl[this.index].className+'" style="'+this.getTransform(0)+'">'+this.tpl[this.index].innerHTML+'</div>'+
+            (this.index<this.tplNum-1?'<div id="i-next" class="js-iSlider-item '+this.tpl[this.index+1].className+'" style="'+this.getTransform('100%')+'">'+this.tpl[this.index+1].innerHTML+'</div>':'');
         this.wrap.style.cssText+="display:block;position:relative;width:100%;height:100%";
 
         if (this.opts.loadingImgs && this.opts.loadingImgs.length) {
@@ -170,37 +180,64 @@ iSlider.prototype={
     pageInit:function () {
         var self = this;
         raf(function () {
-            self.addClass(self.$('#current'),self.opts.playClass);
+            self.addClass(self.$('#i-current'),self.opts.playClass);
         });
+        try {
+            self.opts.onslide.call(self,self.index);
+        } catch (e) {
+            console.info(e)
+        }
     },
 	touchstart : function (e) {
 		if(e.touches.length !== 1){return;}//如果大于1个手指，则不处理
+        
+        this.lockSlide=false;
+        this._touchstartX=e.touches[0].pageX;
+        this._touchstartY=e.touches[0].pageY;
+
 		this.touchInitPos = this.opts.isVertical ? e.touches[0].pageY:e.touches[0].pageX; // 每次move的触点位置
 		this.deltaX1 = this.touchInitPos;//touchstart的时候的原始位置
 
 		this.startPos = 0;
 		this.startPosPrev = -this.scrollDist;
 		this.startPosNext = this.scrollDist;
-		this.hasPrev = !!this.$('#prev');
-		this.hasNext = !!this.$('#next');
+		this.hasPrev = !!this.$('#i-prev');
+		this.hasNext = !!this.$('#i-next');
 		//手指滑动的时候禁用掉动画
 		if (this.hasNext) {
-			this.$('#next').style.cssText+='-webkit-transition-duration:0;'
+			this.$('#i-next').style.cssText+='-webkit-transition-duration:0;'
 		}
 
-		this.$('#current').style.cssText+='-webkit-transition-duration:0;'
+		this.$('#i-current').style.cssText+='-webkit-transition-duration:0;'
 		if (this.hasPrev) {
-			this.$('#prev').style.cssText+='-webkit-transition-duration:0;'
+			this.$('#i-prev').style.cssText+='-webkit-transition-duration:0;'
 		}
 	},
 	touchmove : function (e) {
-		if(e.touches.length !== 1){return;}
+		if(e.touches.length !== 1 || this.lockSlide){return;}
+
+        var gx=Math.abs(e.touches[0].pageX - this._touchstartX);
+        var gy=Math.abs(e.touches[0].pageY - this._touchstartY);
+        
+        //如果手指初始滑动的方向跟页面设置的方向不一致  就不会触发滑动  这个主要是避免误操作, 比如页面是垂直滑动, 在某一页加了横向滑动的局部动画, 那么左右滑动的时候要保证页面不能上下移动. 这里就是做这个的.
+        if (gx>gy && this.opts.isVertical) { //水平滑动
+            this.lockSlide=true;
+            return ;
+        }else if(gx<gy && !this.opts.isVertical){ //垂直滑动
+            this.lockSlide=true;
+            return ;
+        }
+        
+        //如果是禁用了这一页的滑动, 那么往下是划不动了  但是可以往上滑
+        if (this.opts.noslide && this.opts.noslide.indexOf(this.index)>=0 && e.touches[0].pageY - this._touchstartY<0) {
+            return ;
+        }
 
 		var currentX = this.opts.isVertical ? e.touches[0].pageY:e.touches[0].pageX;
 		this.deltaX2 = currentX - this.deltaX1;//记录当次移动的偏移量
 		this.totalDist = this.startPos + currentX - this.touchInitPos;
 
-		this.$('#current').style.cssText+=this.getTransform(this.totalDist+'px');
+		this.$('#i-current').style.cssText+=this.getTransform(this.totalDist+'px');
 		this.startPos = this.totalDist;
 		
 		//处理上一张和下一张
@@ -208,14 +245,14 @@ iSlider.prototype={
 			if (this.hasNext) {
 				this.totalDist2 = this.startPosNext + currentX - this.touchInitPos;
 
-				this.$('#next').style.cssText += this.getTransform(this.totalDist2+'px');
+				this.$('#i-next').style.cssText += this.getTransform(this.totalDist2+'px');
 				this.startPosNext = this.totalDist2;
 			}
 		}else {//露出上一张
 			if (this.hasPrev) {
 				this.totalDist2 = this.startPosPrev + currentX - this.touchInitPos;
 
-				this.$('#prev').style.cssText += this.getTransform(this.totalDist2+'px');
+				this.$('#i-prev').style.cssText += this.getTransform(this.totalDist2+'px');
 				this.startPosPrev = this.totalDist2;
 			}
 		}
@@ -233,22 +270,24 @@ iSlider.prototype={
 		this.deltaX2 = 0;
 	},
     getTransform:function (dist) {
-        return ';-webkit-transform:translate3d('+(this.opts.isVertical? '0,'+dist : dist+',0')+',0)';
+        var pos= this.opts.isVertical? '0,'+dist : dist+',0';
+        return ';-webkit-transform:' + (this.opts.useACC ? 'translate3d('+pos+',0)' : 'translate('+pos+')');
     },
+
     itemReset:function () {
-        this.$('#current').style.cssText+='-webkit-transition-duration:'+this.opts.speed+'ms;'+this.getTransform(0);
-        if (this.$('#prev')) {
-            this.$('#prev').style.cssText+='-webkit-transition-duration:'+this.opts.speed+'ms;'+this.getTransform('-'+this.scrollDist+'px');
+        this.$('#i-current').style.cssText+='-webkit-transition-duration:'+this.opts.speed+'ms;'+this.getTransform(0);
+        if (this.$('#i-prev')) {
+            this.$('#i-prev').style.cssText+='-webkit-transition-duration:'+this.opts.speed+'ms;'+this.getTransform('-'+this.scrollDist+'px');
         }
-        if (this.$('#next')) {
-           this.$('#next').style.cssText+='-webkit-transition-duration:'+this.opts.speed+'ms;'+this.getTransform(this.scrollDist+'px');
+        if (this.$('#i-next')) {
+           this.$('#i-next').style.cssText+='-webkit-transition-duration:'+this.opts.speed+'ms;'+this.getTransform(this.scrollDist+'px');
         }
 		this.deltaX2 = 0;
     },
 
     prev:function () {
         var self = this;
-        if (!this.$('#current') || !this.$('#prev')) {
+        if (!this.$('#i-current') || !this.$('#i-prev')) {
             this.itemReset();
             return ;
         }
@@ -261,20 +300,23 @@ iSlider.prototype={
 
         var nextIndex = this.index+1 > this.tplNum-1 ? 0 : this.index+1;
 
-        if (this.$('#next')) {
-            this.wrap.removeChild(this.$('#next'));
+        if (this.$('#i-next')) {
+            this.wrap.removeChild(this.$('#i-next'));
         }
-        this.$('#current').id='next';
-        this.$('#prev').id='current';
-        this.$('#next').style.cssText+='-webkit-transition-duration:'+this.opts.speed+'ms;'+this.getTransform(this.scrollDist+'px');
-        this.$('#current').style.cssText+='-webkit-transition-duration:'+this.opts.speed+'ms;'+this.getTransform(0);
+        this.$('#i-current').id='i-next';
+        this.$('#i-prev').id='i-current';
+        this.$('#i-next').style.cssText+='-webkit-transition-duration:'+this.opts.speed+'ms;'+this.getTransform(this.scrollDist+'px');
+        this.$('#i-current').style.cssText+='-webkit-transition-duration:'+this.opts.speed+'ms;'+this.getTransform(0);
+
+        sessionStorage[sessionKey]=this.index;
 
         raf(function () {
 
-            if (self.$('.play')) {
-                self.$('.play').className=self.$('.play').className.replace(/\s*\bplay\b/g,'');
+            if (self.$('.'+self.opts.playClass)) {
+                self.removeClass(self.$('.'+self.opts.playClass),self.opts.playClass)
             }
-            self.$('#current').className +=' play';
+            self.addClass(self.$('#i-current'),self.opts.playClass)
+
             try {
                 self.opts.onslide.call(self,self.index);
             } catch (e) {
@@ -289,12 +331,12 @@ iSlider.prototype={
 
             var addItem = document.createElement('div');
             addItem.className='js-iSlider-item '+self.tpl[prevIndex].className;
-            addItem.id='prev';
+            addItem.id='i-prev';
             addItem.style.cssText+='-webkit-transition-duration:0ms;'+self.getTransform('-'+self.scrollDist+'px');
 
             addItem.innerHTML=self.tpl[prevIndex].innerHTML;
 
-            self.wrap.insertBefore(addItem,self.$('#current'));
+            self.wrap.insertBefore(addItem,self.$('#i-current'));
 
         })
 
@@ -302,7 +344,7 @@ iSlider.prototype={
 
     next:function () {
         var self = this;
-        if (!this.$('#current') || !this.$('#next')) {
+        if (!this.$('#i-current') || !this.$('#i-next')) {
             this.itemReset();
             return ;
         }
@@ -316,20 +358,20 @@ iSlider.prototype={
         
         var prevIndex = this.index===0 ? this.tplNum-1 : this.index-1;
 
-        if (this.$('#prev')) {
-            this.wrap.removeChild(this.$('#prev'));
+        if (this.$('#i-prev')) {
+            this.wrap.removeChild(this.$('#i-prev'));
         }
-        this.$('#current').id='prev';
-        this.$('#next').id='current';
-        this.$('#prev').style.cssText+='-webkit-transition-duration:'+this.opts.speed+'ms;'+this.getTransform('-'+this.scrollDist+'px');
-        this.$('#current').style.cssText+='-webkit-transition-duration:'+this.opts.speed+'ms;'+this.getTransform(0);
-       
+        this.$('#i-current').id='i-prev';
+        this.$('#i-next').id='i-current';
+        this.$('#i-prev').style.cssText+='-webkit-transition-duration:'+this.opts.speed+'ms;'+this.getTransform('-'+this.scrollDist+'px');
+        this.$('#i-current').style.cssText+='-webkit-transition-duration:'+this.opts.speed+'ms;'+this.getTransform(0);
+        sessionStorage[sessionKey]=this.index;
         raf(function () {
 
-            if (self.$('.play')) {
-                self.$('.play').className=self.$('.play').className.replace(/\s*\bplay\b/g,'');
+            if (self.$('.'+self.opts.playClass)) {
+                self.removeClass(self.$('.'+self.opts.playClass),self.opts.playClass)
             }
-            self.$('#current').className +=' play';
+            self.addClass(self.$('#i-current'),self.opts.playClass)
 
             try {
                 self.opts.onslide.call(self,self.index);
@@ -344,7 +386,7 @@ iSlider.prototype={
 
             var addItem = document.createElement('div');
             addItem.className='js-iSlider-item '+self.tpl[nextIndex].className;
-            addItem.id='next';
+            addItem.id='i-next';
             addItem.style.cssText+='-webkit-transition-duration:0ms;'+self.getTransform(self.scrollDist+'px');
             addItem.innerHTML=self.tpl[nextIndex].innerHTML;
 
